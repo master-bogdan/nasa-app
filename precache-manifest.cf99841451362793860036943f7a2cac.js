0bc4938bac8b60521e506958ee922b846f9c4d2b
masterbogdan <maiskiychavk@gmail.com>
2020-08-19 22:14:41 +0300

    Updates

---
+++ precache-manifest.cf99841451362793860036943f7a2cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c833bbd0ebab4133a0618c04cc9c43d1",
+    "url": "/nasa-app/index.html"
+  },
+  {
+    "revision": "0f0a8204ca5200e861a0",
+    "url": "/nasa-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59d4a491a6dfaae4c6cb",
+    "url": "/nasa-app/static/js/2.55314111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nasa-app/static/js/2.55314111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0a8204ca5200e861a0",
+    "url": "/nasa-app/static/js/main.3471924b.chunk.js"
+  },
+  {
+    "revision": "c3970e2f9a98ce78f24e",
+    "url": "/nasa-app/static/js/runtime-main.1c4d3ad0.js"
+  }
+]);
